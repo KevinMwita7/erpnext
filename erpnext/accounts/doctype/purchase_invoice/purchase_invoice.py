10d7482f26ce81bc0d2ce17213ea525b106571a0
KevinMwita7 <33288033+KevinMwita7@users.noreply.github.com>
2020-09-26 19:49:25 +0300

    Update purchase_invoice.py

--- erpnext/accounts/doctype/purchase_invoice/purchase_invoice.py
+++ erpnext/accounts/doctype/purchase_invoice/purchase_invoice.py
@@ -889,7 +889,8 @@ def make_debit_note(source_name, target_doc=None):
 @frappe.whitelist()
 def make_stock_entry(source_name, target_doc=None):
 	def update_item(obj, target, source_parent):
-		frappe.msgprint("<pre>{}</pre>".format(frappe.as_json(source_parent)))
+		if(source_parent.supplier):
+			target.supplier = source_parent.supplier
 
 	doc = get_mapped_doc("Purchase Invoice", source_name, {
 		"Purchase Invoice": {
