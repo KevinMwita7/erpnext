558d65a2675f0549bb9bc74206f2f4162f4c465c
KevinMwita7 <33288033+KevinMwita7@users.noreply.github.com>
2020-09-27 17:31:44 +0300

    Update issue_and_receipt_voucher.js

--- erpnext/stock/doctype/issue_and_receipt_voucher/issue_and_receipt_voucher.js
+++ erpnext/stock/doctype/issue_and_receipt_voucher/issue_and_receipt_voucher.js
@@ -3,6 +3,39 @@
 
 frappe.ui.form.on('Issue and Receipt Voucher', {
 	refresh: function(frm) {
+		const me = this;
+		this.frm.add_custom_button(__('Purchase Order'), function() {
+			erpnext.utils.map_current_doc({
+				method: "erpnext.buying.doctype.purchase_order.purchase_order.make_purchase_invoice",
+				source_doctype: "Purchase Order",
+				target: me.frm,
+				setters: {
+					// supplier: me.frm.doc.supplier || undefined,
+				},
+				/*get_query_filters: {
+					docstatus: 1,
+					status: ["!=", "Closed"],
+					per_billed: ["<", 99.99],
+					company: me.frm.doc.company
+				}*/
+			})
+		}, __("Get items from"));
 
-	}
+		this.frm.add_custom_button(__('Material Request'), function() {
+			erpnext.utils.map_current_doc({
+				method: "erpnext.buying.doctype.purchase_order.purchase_order.make_purchase_invoice",
+				source_doctype: "Purchase Order",
+				target: me.frm,
+				setters: {
+					// supplier: me.frm.doc.supplier || undefined,
+				},
+				/*get_query_filters: {
+					docstatus: 1,
+					status: ["!=", "Closed"],
+					per_billed: ["<", 99.99],
+					company: me.frm.doc.company
+				}*/
+			})
+		}, __("Get items from"));		
+	},
 });
