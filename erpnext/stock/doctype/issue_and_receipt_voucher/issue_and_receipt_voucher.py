8390a1b85740bdc3edc5f16e86b1266b8fd667a5
KevinMwita7 <33288033+KevinMwita7@users.noreply.github.com>
2020-09-27 18:32:31 +0300

    Update issue_and_receipt_voucher.py

--- erpnext/stock/doctype/issue_and_receipt_voucher/issue_and_receipt_voucher.py
+++ erpnext/stock/doctype/issue_and_receipt_voucher/issue_and_receipt_voucher.py
@@ -7,34 +7,36 @@ import frappe
 from frappe.model.document import Document
 
 class IssueandReceiptVoucher(Document):
-	@frappe.whitelist()
-	def get_items_from_purchase_order(source_name, target_doc=None):
-		frappe.msgprint("<pre>{}</pre>".format(frappe.as_json(source_name)))
-		def update_item(obj, target, source_parent):
-			pass
-		
-		def set_missing_values(source, target):
-			frappe.msgprint("<pre>{}</pre>".format(frappe.as_json(source)))
-			frappe.msgprint("<pre>{}</pre>".format(frappe.as_json(target)))
-			target.issued_date = frappe.utils.nowdate()
+	pass
 
-		doc = get_mapped_doc("Purchase Order", source_name, {
-			"Purchase Invoice": {
-				"doctype": "Issue and Receipt Voucher",
-				"validation": {
-					"docstatus": ["=", 1]
-				}
-			},
-			"Purchase Invoice Item": {
-				"doctype": "Issue and Receipt Voucher Item",
-				"field_map": {
-					# Source      Target
-					"item_code": "item_description",
-					"uom": "batch_no",
-					"qty": "qty_required"
-				},
-				# "postprocess": update_item,
+@frappe.whitelist()
+def get_items_from_purchase_order(source_name, target_doc=None):
+	frappe.msgprint("<pre>{}</pre>".format(frappe.as_json(source_name)))
+	def update_item(obj, target, source_parent):
+		pass
+	
+	def set_missing_values(source, target):
+		frappe.msgprint("<pre>{}</pre>".format(frappe.as_json(source)))
+		frappe.msgprint("<pre>{}</pre>".format(frappe.as_json(target)))
+		target.issued_date = frappe.utils.nowdate()
+
+	doc = get_mapped_doc("Purchase Order", source_name, {
+		"Purchase Invoice": {
+			"doctype": "Issue and Receipt Voucher",
+			"validation": {
+				"docstatus": ["=", 1]
 			}
-		}, target_doc, set_missing_values)
+		},
+		"Purchase Invoice Item": {
+			"doctype": "Issue and Receipt Voucher Item",
+			"field_map": {
+				# Source      Target
+				"item_code": "item_description",
+				"uom": "batch_no",
+				"qty": "qty_required"
+			},
+			# "postprocess": update_item,
+		}
+	}, target_doc, set_missing_values)
 
-		return doc
+	return doc
