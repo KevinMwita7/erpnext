d3feddb000c50f508bccfc25bf9145b897e8f0b6
KevinMwita7 <33288033+KevinMwita7@users.noreply.github.com>
2020-09-27 17:45:17 +0300

    Update issue_and_receipt_voucher.js

--- erpnext/stock/doctype/issue_and_receipt_voucher/issue_and_receipt_voucher.js
+++ erpnext/stock/doctype/issue_and_receipt_voucher/issue_and_receipt_voucher.js
@@ -3,37 +3,36 @@
 
 frappe.ui.form.on('Issue and Receipt Voucher', {
 	refresh: function(frm) {
-		const me = this;
-		this.frm.add_custom_button(__('Purchase Order'), function() {
+		frm.add_custom_button(__('Purchase Order'), function() {
 			erpnext.utils.map_current_doc({
 				method: "erpnext.buying.doctype.purchase_order.purchase_order.make_purchase_invoice",
 				source_doctype: "Purchase Order",
-				target: me.frm,
+				target: frm,
 				setters: {
-					// supplier: me.frm.doc.supplier || undefined,
+					// supplier: frm.doc.supplier || undefined,
 				},
 				/*get_query_filters: {
 					docstatus: 1,
 					status: ["!=", "Closed"],
 					per_billed: ["<", 99.99],
-					company: me.frm.doc.company
+					company: frm.doc.company
 				}*/
 			})
 		}, __("Get items from"));
 
-		this.frm.add_custom_button(__('Material Request'), function() {
+		frm.add_custom_button(__('Material Request'), function() {
 			erpnext.utils.map_current_doc({
 				method: "erpnext.buying.doctype.purchase_order.purchase_order.make_purchase_invoice",
 				source_doctype: "Purchase Order",
-				target: me.frm,
+				target: frm,
 				setters: {
-					// supplier: me.frm.doc.supplier || undefined,
+					// supplier: frm.doc.supplier || undefined,
 				},
 				/*get_query_filters: {
 					docstatus: 1,
 					status: ["!=", "Closed"],
 					per_billed: ["<", 99.99],
-					company: me.frm.doc.company
+					company: frm.doc.company
 				}*/
 			})
 		}, __("Get items from"));		
