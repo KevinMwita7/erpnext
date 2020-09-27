69b42893a53599b3d3801d279d702e86acb8a616
KevinMwita7 <33288033+KevinMwita7@users.noreply.github.com>
2020-09-27 18:37:14 +0300

    Update issue_and_receipt_voucher.py

--- erpnext/stock/doctype/issue_and_receipt_voucher/issue_and_receipt_voucher.py
+++ erpnext/stock/doctype/issue_and_receipt_voucher/issue_and_receipt_voucher.py
@@ -5,6 +5,7 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe.model.mapper import get_mapped_doc
 
 class IssueandReceiptVoucher(Document):
 	pass
