0648f72e1328fefb01bc8818732a8383fb437128
KevinMwita7 <33288033+KevinMwita7@users.noreply.github.com>
2020-09-29 13:56:00 +0300

    Update stock_ledger.py

--- erpnext/stock/report/stock_ledger/stock_ledger.py
+++ erpnext/stock/report/stock_ledger/stock_ledger.py
@@ -29,7 +29,7 @@ def execute(filters=None):
 			conversion_factors.append(item_detail.conversion_factor)
 
 	update_included_uom_in_report(columns, data, include_uom, conversion_factors)
-	frappe.msgprint("<pre>{}</pre>".format(frappe.as_json(data)))
+	frappe.msgprint(sl_entries)
 	return columns, data
 
 def get_columns():
