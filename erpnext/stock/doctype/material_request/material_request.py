71cc204bf39a2262af9bc279cadede975f6fcd70
KevinMwita7 <33288033+KevinMwita7@users.noreply.github.com>
2020-10-07 08:37:42 +0300

    Update material_request.py

--- erpnext/stock/doctype/material_request/material_request.py
+++ erpnext/stock/doctype/material_request/material_request.py
@@ -522,7 +522,7 @@ def make_material_receipt(source_name, target_doc=None):
 					temp["s_warehouse"] = source.source_warehouse
 			else:
 				temp["s_warehouse"] = item.warehouse
-			# items.append(temp)
+			items.append(temp)
 
 		target.items = items
 		target.purpose = source.material_request_type
