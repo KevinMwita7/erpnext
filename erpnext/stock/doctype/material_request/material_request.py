acf2f209fe8ed69e6459b15b454e1d0fe6de8a6c
KevinMwita7 <33288033+KevinMwita7@users.noreply.github.com>
2020-10-08 18:36:05 +0300

    Update material_request.py

--- erpnext/stock/doctype/material_request/material_request.py
+++ erpnext/stock/doctype/material_request/material_request.py
@@ -466,7 +466,7 @@ def make_stock_entry(source_name, target_doc=None):
 				target.s_warehouse = source_parent.source_warehouse
 		else:
 			target.s_warehouse = obj.warehouse
-		frappe.msgprint("<pre>{}</pre>".format(frappe.as_json(target)))
+		frappe.msgprint("<pre>{}</pre>".format(frappe.as_json(get_bin_details(target.item_code, target.s_warehouse))))
 		# Get the stock of the item in the source warehouse
 		target.actual_qty = get_bin_details(target.item_code, target.s_warehouse).actual_qty
 
