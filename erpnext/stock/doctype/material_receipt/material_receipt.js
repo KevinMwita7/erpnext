1a8a6d3f218083411497497e1f3036219600dc89
KevinMwita7 <33288033+KevinMwita7@users.noreply.github.com>
2020-10-15 10:23:26 +0300

    Update material_receipt.js

--- erpnext/stock/doctype/material_receipt/material_receipt.js
+++ erpnext/stock/doctype/material_receipt/material_receipt.js
@@ -3,8 +3,10 @@
 
 frappe.ui.form.on('Material Receipt', {
 	onload: function(frm) {
-		cur_frm.add_child("items");
-		cur_frm.refresh_field("items");
+		if(!frm.doc.items.length) {
+			cur_frm.add_child("items");
+			cur_frm.refresh_field("items");
+		}
 	},
 	onload_post_render: function(frm) {
 		frm.get_field("items").grid.set_multiple_add("item_code", "qty");
