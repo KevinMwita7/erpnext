893dd188be4eafdfcee3792ba468543744f87cd2
KevinMwita7 <33288033+KevinMwita7@users.noreply.github.com>
2020-11-16 13:42:31 +0300

    Update stock_ledger.py

--- erpnext/stock/report/stock_ledger/stock_ledger.py
+++ erpnext/stock/report/stock_ledger/stock_ledger.py
@@ -34,8 +34,8 @@ def execute(filters=None):
 		#frappe.msgprint("<pre>{}</pre>".format(frappe.as_json(data)))
 		data = list(filter(lambda el : True if el.supplier == filters.get("supplier") else False, data))
 	# Filter warehouses
-	#if(filters.get("warehouse")):
-		#frappe.msgprint("<pre>{}</pre>".format(frappe.as_json(data)))
+	if(filters.get("warehouse")):
+		frappe.msgprint("<pre>{}</pre>".format(frappe.as_json(data)))
 		#data = list(filter(lambda el : True if el.warehouse == filters.get("warehouse") else False, data))
 
 	return columns, data
