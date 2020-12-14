6b8ebc75eebd1ada132062633c317759b733f8d3
KevinMwita7 <33288033+KevinMwita7@users.noreply.github.com>
2020-12-14 13:57:59 +0300

    Update workload.py

--- erpnext/accounts/report/workload/workload.py
+++ erpnext/accounts/report/workload/workload.py
@@ -49,5 +49,5 @@ def get_conditions(filters):
 		conditions += "transaction_date >= '%s'" % filters.get("from_date")
 	if filters.get("to_date"):
 		conditions += " and transaction_date <= '%s" % filters.get("to_date")
-	print(conditions)
+	frappe.msgprint(conditions)
 	return conditions
