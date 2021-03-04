da95c6acfc45d793fbe9a2cc23fbc4cf174df56c
KevinMwita7 <33288033+KevinMwita7@users.noreply.github.com>
2021-03-04 10:44:57 +0300

    Update sales_invoice.py

--- erpnext/accounts/doctype/sales_invoice/sales_invoice.py
+++ erpnext/accounts/doctype/sales_invoice/sales_invoice.py
@@ -203,7 +203,7 @@ class SalesInvoice(SellingController):
 		if "Healthcare" in active_domains:
 			manage_invoice_submit_cancel(self, "on_submit")
 
-		if hasattr(self, "is_dev") or hasattr(self, "is_test") and self.charge_type == "Registration Fee Payment" and \
+		if hasattr(self, "is_dev") or hasattr(self, "is_test") or hasattr(self, "is_prod") and self.charge_type == "Registration Fee Payment" and \
 			hasattr(self, "customer_uuid") and self.customer_uuid and self.status == "Paid":
 			# Suppress only the single warning from urllib3 needed.
 			requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
@@ -220,9 +220,9 @@ class SalesInvoice(SellingController):
 				#frappe.msgprint("<pre>{}</pre>".format(frappe.as_json(r.json())))
 			elif(hasattr(self, "is_dev")):
 				r = requests.post("https://159.89.10.195/openmrs/ws/rest/v1/visit", data=json.dumps(payload), headers=headers, auth=HTTPBasicAuth('admin', 'test'), verify=False)
-				#frappe.msgprint("<pre>{}</pre>".format(frappe.as_json(r.json())))
+				frappe.msgprint("<pre>{}</pre>".format(frappe.as_json(r.json())))
 			else:
-				requests.post("https://142.93.41.252/openmrs/ws/rest/v1/visit", data=json.dumps(payload), headers=headers, auth=HTTPBasicAuth('admin', 'test'), verify=False)
+				requests.post("https://157.230.13.78/openmrs/ws/rest/v1/visit", data=json.dumps(payload), headers=headers, auth=HTTPBasicAuth('admin', 'test'), verify=False)
 
 	def validate_pos_paid_amount(self):
 		if len(self.payments) == 0 and self.is_pos:
