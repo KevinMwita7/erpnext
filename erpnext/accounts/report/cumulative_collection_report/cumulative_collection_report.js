264969abeccbae020ec8e300951d6eaee02c1944
KevinMwita7 <33288033+KevinMwita7@users.noreply.github.com>
2021-05-06 11:14:42 +0300

    Update cumulative_collection_report.js

--- erpnext/accounts/report/cumulative_collection_report/cumulative_collection_report.js
+++ erpnext/accounts/report/cumulative_collection_report/cumulative_collection_report.js
@@ -4,13 +4,19 @@ frappe.query_reports["Cumulative Collection Report"] = {
 			"fieldname":"from_date",
 			"label": __("From Date"),
 			"fieldtype": "Date",
-			"default": frappe.datetime.get_today()
+			"default": "2020-10-26"
 		},
         {
 			"fieldname":"to_date",
 			"label": __("To Date"),
 			"fieldtype": "Date",
 			"default": frappe.datetime.get_today()
-		},        
+		},
+        {
+			"fieldname":"collection_type",
+			"label": __("Collection Type"),
+			"fieldtype": "Data",
+			"default": ""
+		},
     ]
 };
