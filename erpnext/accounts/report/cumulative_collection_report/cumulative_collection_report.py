a169cf93d012f4021b80ac0c5a9ca748e5a5e6ce
KevinMwita7 <33288033+KevinMwita7@users.noreply.github.com>
2021-05-06 15:07:14 +0300

    Update cumulative_collection_report.py

--- erpnext/accounts/report/cumulative_collection_report/cumulative_collection_report.py
+++ erpnext/accounts/report/cumulative_collection_report/cumulative_collection_report.py
@@ -14,6 +14,7 @@ def get_data(filters):
 	from_date = filters["from_date"] if "from_date" in filters else "26-10-2020"
 	to_date = filters["to_date"] if "to_date" in  filters else datetime.now().strftime('%Y-%m-%d')
 	collection_type = filters["collection_type"] if "collection_type" in filters else ''
+	msgprint(collection_type)
 
 	cumulative_data = db.sql("""
 	SELECT sum(base_total) as total_sum, IF(remarks='No Remarks',"Others",remarks) as remarks
