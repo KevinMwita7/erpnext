901ecfd50548285706ea39b787747578c6dad4aa
KevinMwita7 <33288033+KevinMwita7@users.noreply.github.com>
2021-05-06 13:54:55 +0300

    Update cumulative_collection_report.py

--- erpnext/accounts/report/cumulative_collection_report/cumulative_collection_report.py
+++ erpnext/accounts/report/cumulative_collection_report/cumulative_collection_report.py
@@ -5,5 +5,34 @@ from __future__ import unicode_literals
 import frappe
 
 def execute(filters=None):
-	columns, data = [], []
+	columns, data = get_columns(), get_data()
 	return columns, data
+
+def get_data(filters):
+	frappe.msgprint("<pre>{}</pre>".format(frappe.as_json(filters)))
+	cumulative_data = frappe.db.sql("""
+	SELECT sum(base_total), if(remarks='No Remarks',"Others",remarks) 
+	FROM 4fc07e5444f76a8c.`tabSales Invoice` group by remarks;
+	""")
+
+	data = [
+		[
+			row['base_total'],
+			row['remarks']
+		] for row in cumulative_data]
+
+	return data	
+
+def get_columns():
+	return [{
+		"fieldname": "total_amount",
+		"label": _("Total Amount"),
+		"fieldtype": "Data",
+		"width": 550
+	},
+	{
+		"fieldname": "collection_type",
+		"label": _("Collection Type"),
+		"fieldtype": "Data",
+		"width": 550
+	}]
