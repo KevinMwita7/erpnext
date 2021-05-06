47135916bc322e5f69bae15bb7acc2e3e0bc7e2e
KevinMwita7 <33288033+KevinMwita7@users.noreply.github.com>
2021-05-06 11:06:03 +0300

    Update cumulative_collection_report.js

--- erpnext/accounts/report/cumulative_collection_report/cumulative_collection_report.js
+++ erpnext/accounts/report/cumulative_collection_report/cumulative_collection_report.js
@@ -1,13 +1,13 @@
 frappe.query_reports["Cumulative Collection Report"] = {
     "filters": [
         {
-			"fieldname":"creation",
+			"fieldname":"from_date",
 			"label": __("From Date"),
 			"fieldtype": "Date",
 			"default": frappe.datetime.get_today()
 		},
         {
-			"fieldname":"creation",
+			"fieldname":"to_date",
 			"label": __("To Date"),
 			"fieldtype": "Date",
 			"default": frappe.datetime.get_today()
